f282412005ca2a780dc217bc00783f1e065fe07d
benlittlenz <benlittle53@gmail.com>
2021-06-12 13:17:31 +1200

    implemented layout to inject into views

--- core/Router.php
+++ core/Router.php
@@ -38,11 +38,22 @@ class Router
   public function renderView($view)
   {
     $layoutContent = $this->layoutContent();
+    $viewContent = $this->renderOnlyView($view);
+    return str_replace('{{content}}', $viewContent, $layoutContent);
     include_once Application::$ROOT_DIR."/views/$view.php";
   }
 
   protected function layoutContent()
   {
+    ob_start();
     include_once Application::$ROOT_DIR."/views/layouts/main.php";
+    return ob_get_clean();
+  }
+
+  protected function renderOnlyView($view)
+  {
+    ob_start();
+    include_once Application::$ROOT_DIR."/views/$view.php";
+    return ob_get_clean();
   }
 }
--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use app\core\Application;
 require_once __DIR__.'/../vendor/autoload.php';
 
